d99ac3a51793f6e816250a64d03ace7bdbec1e9e
parth panchal <parth6606@gmail.com>
2020-05-08 14:35:53 +0530

    Created Table Match

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Match(models.Model):
+	location = models.CharField(max_length = 64)
+	sport = models.CharField(max_length = 64)
+	maxPlayers = models.IntegerField()
